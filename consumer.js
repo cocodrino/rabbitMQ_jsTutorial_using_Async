5450c874a9a2f1def6ce2aa0016ce84a1341fca8
Carlos LM <clagccs@gmail.com>
2018-12-09 22:05:13 -0400

    init pubsub code

--- consumer.js
+++ consumer.js
@@ -2,6 +2,7 @@ const q = "tasks";
 
 async function consumer() {
   try {
+    console.log("[*] Connecting");
     let con = await require("amqplib").connect(
       "amqp://tvobttnq:Ri6S8XroB9gRiIvSesqQMa2KcWI_IOkB@otter.rmq.cloudamqp.com/tvobttnq"
     );
